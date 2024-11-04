c7e60980a1842515177389106efa856cc4c22530
GrzegorzWikar <grzegorzwikar938@gmail.com>
2024-11-04 13:03:59 +0100

    TytulyTabeli

--- WentylacjaMechaniczna/MainPage.xaml.cs
+++ WentylacjaMechaniczna/MainPage.xaml.cs
@@ -1,18 +1,81 @@
-﻿namespace WentylacjaMechaniczna
+﻿using Microsoft.Maui.Controls.Shapes;
+
+namespace WentylacjaMechaniczna
 {
     public partial class MainPage : ContentPage
     {
+        string[] TableHeaders = ["L.P", "Nazwa Pomieszczenia", "Powierzchnia [m2]", "Wysokosc pomieszczenia", "Kubatura [m3]", "Krotnosc Wymian [1/h]", "Obliczeniowy strumien powietrza nawiewanego [m3/h]", "Obliczeniowy strumien powietrza wywiewanego [m3/h]", "Przyjety strumien powietrza nawiewanego [m3/h]", "Przyjety strumien powietrza wywiewanego [m3/h]"];
+
         public MainPage()
         {
             InitializeComponent();
 
-            var rooms = new List<Room> 
+            ColumnDefinitionCollection columns = new ColumnDefinitionCollection();
+            RowDefinitionCollection rows = new RowDefinitionCollection();
+            foreach (string column in TableHeaders)
             {
-                new Room("Kuchnia", 20.5M, 2.5M )
+                columns.Add(new ColumnDefinition());
+                rows.Add(new RowDefinition());
+            }
+
+            rows[0].Height = 100;
+            columns[0].Width = 60;
+
+            Grid mainGrid = new Grid()
+            {
+                ColumnDefinitions = columns,
+                RowDefinitions = rows,
             };
 
-           //homeView.ItemsSource = rooms;
+            mainGrid.Margin = 5;
 
+            HorizontalStackLayout tableHeaders = [];
+            tableHeaders.VerticalOptions = LayoutOptions.Start;
+            HorizontalStackLayout tableRow = [];
+
+
+            mainGrid.Add(tableHeaders);
+            mainGrid.Add(tableRow);
+           
+
+            foreach (string header in TableHeaders)
+            {
+                
+                Border headerBorder = newBorder();
+
+                Label label = new() {
+                    HorizontalOptions = LayoutOptions.Center,
+                    VerticalOptions = LayoutOptions.Center,
+                    Text = header,
+                    TextColor = Colors.White,
+                };
+
+                headerBorder.Content = label;
+            
+                mainGrid.Add(headerBorder, Array.IndexOf(TableHeaders,header), 0);
+
+            }
+
+            Content = mainGrid;
+        }
+
+        private Border newBorder()
+        {
+            Border mainBorder = new Border()
+            {
+                Stroke = Colors.DarkGoldenrod,
+                BackgroundColor = Colors.DarkBlue,
+                StrokeThickness = 4,
+                StrokeShape = new RoundRectangle
+                {
+                    CornerRadius = new CornerRadius(40, 0, 0, 40)
+                }
+              ,
+                Padding = new Thickness(16, 8),
+                HorizontalOptions = LayoutOptions.Center,
+            };
+
+            return mainBorder;
         }
     }
 }
